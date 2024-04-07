adba73bc24fd679e74c74bad1c2ade48571ed835
armandotapiach <armando.tapia@kushkipagos.com>
2024-04-06 22:16:00 -0500

    fix runtime

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,5 +1,5 @@
 export async function handler(event: string, context: string) {
-  console.log("Stage name is: " + process);
+  // console.log("Stage name is: " + process.env.stage);
   return {
     body: "Hello from a lambda Functions",
     statusCode: 200,
