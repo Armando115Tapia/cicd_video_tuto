cb47c43632ab8cbc12a0926f2f7205fb8be87886
armandotapiach <armando.tapia@kushkipagos.com>
2024-04-06 22:20:27 -0500

    redeploy

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,7 +1,7 @@
 export async function handler(event: string, context: string) {
   // console.log("Stage name is: " + process.env.stage);
   return {
-    body: "Hello from a lambda Functions",
+    body: "Hello from a lambda Functions 001",
     statusCode: 200,
   };
 }
